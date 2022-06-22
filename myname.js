632d41d10e4189fbd915b945944c89fb20233258
Adejohn0001 <104025025+Adejohn0001@users.noreply.github.com>
2022-06-22 13:26:17 +0100

    Update myname.js
    updated

--- myname.js
+++ myname.js
@@ -3,16 +3,7 @@ var lastName = "Ojo";
 var height = 4.5;
 var country = "Nigeria";
 
-let allDisplay = "My name is " +
-firstName +
-" " +
-lastName +
-". I am " +
-height +
-"ft tall" +
-", and I am from " +
-country +
-".";
+let allDisplay = "My name is " + firstName + " " + lastName + ". I am " + height + "ft tall" + ", and I am from " + country + ".";
 
 document.getElementById("display").innerHTML = allDisplay;
 
